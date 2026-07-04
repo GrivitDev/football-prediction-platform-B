0a91c26da49940bea219e4ad7ff0f50d09bfdbf5
GrivitDev <grivitdev@gmail.com>
2026-07-04 19:01:13 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://your-vercel-domain.vercel.app'],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
 
